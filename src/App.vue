9d4d75a60d3222476f22ea4ce3ba44a7fb29b35d
serii <seriiburduja@gmail.com>
2021-03-22 08:13:49 +0200

    app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
-  <input type="text" v-model="title">
-  <hr>
-  <h3>{{ title }}</h3>
+  <input type="text" v-model="promo">
+  <div v-if="hasSale">
+    <div class="base">base: {{price}}</div>
+    <div class="final">total: {{total}} -{{sale}}%</div>
+  </div>
+  <div class="final" v-else>total: {{total}}</div>
 </template>
 
 <script>
-
+function getSale (promo) {
+  const promos = { some: 10, final: 20 }
+  return Object.prototype.hasOwnProperty.call(promos, promo) ? promos[promo] : 0
+}
 export default {
   name: 'App',
   data () {
     return {
-      title: ''
+      promo: '',
+      price: 1000
+    }
+  },
+  computed: {
+    sale () {
+      return getSale(this.promo)
+    },
+    hasSale () {
+      return this.sale > 0
+    },
+    total () {
+      const sale = getSale(this.promo)
+      return this.price - (sale / 100 * this.price)
     }
   }
 }
