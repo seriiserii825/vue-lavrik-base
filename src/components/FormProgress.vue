33b99bdcd96b197540eea30baf974cf3f3c74fdd
serii <seriiburduja@gmail.com>
2021-03-24 13:27:48 +0200

    progress bar

--- src/components/FormProgress.vue
+++ src/components/FormProgress.vue
@@ -1,29 +1,29 @@
 <template>
-  <form>
+  <form @submit.prevent="onSubmit">
     <div class="progress">
-      <div class="progress-bar"></div>
+      <div class="progress-bar" :style="progressWidth">{{ fieldsDone }}</div>
     </div>
     <div>
-      <div class="form-group" v-for="(field) in info" :key="field.name">
+      <div class="form-group" v-for="(field, i) in info" :key="field.name">
         <label>{{ field.name }}</label>
-        <input type="text" class="form-control" v-model="field.value">
+        <span
+          v-if="field.isIconVisible"
+          class="fa"
+          :class="field.valid ? 'fa-check-circle text-success' : 'fa-exclamation-circle text-danger'"
+        ></span>
+        <input type="text" class="form-control" @input="onInput($event.target.value, i)" :value="field.value">
       </div>
     </div>
-    <button class="btn btn-primary">
+    <button class="btn btn-primary" :disabled="!formIsValid">
       Send Data
     </button>
-    <pre>
-    {{ info }}
-    </pre>
   </form>
-  <div>
-    <table class="table table-bordered">
-      <tr>
-        <td></td>
-        <td></td>
-      </tr>
-    </table>
-  </div>
+  <table class="table table-bordered" v-if="isSubmited">
+    <tr v-for="item in info" :key="item.name">
+      <td>{{ item.name }}</td>
+      <td>{{ item.value }}</td>
+    </tr>
+  </table>
 </template>
 
 <script>
@@ -34,40 +34,65 @@ export default {
         {
           name: 'Name',
           value: '',
-          pattern: /^[a-zA-Z ]{2,30}$/,
-          activated: false, // is visibble icon
-          valid: false // is valid input
+          pattern: /^[a-zA-Z ]{2,30}$/
         },
         {
           name: 'Phone',
           value: '',
-          pattern: /^[0-9]{7,14}$/,
-          activated: false, // is visibble icon
-          valid: false // is valid input
+          pattern: /^[0-9]{7,14}$/
         },
         {
           name: 'Email',
           value: '',
-          pattern: /.+/,
-          activated: false, // is visibble icon
-          valid: false // is valid input
+          pattern: /.+/
         },
         {
           name: 'Some Field 1',
           value: '',
-          pattern: /.+/,
-          activated: false, // is visibble icon
-          valid: false // is valid input
+          pattern: /.+/
         },
         {
           name: 'Some Field 2',
           value: '',
-          pattern: /.+/,
-          activated: false, // is visibble icon
-          valid: false // is valid input
+          pattern: /.+/
         }
-      ]
+      ],
+      isSubmited: false
     }
+  },
+  computed: {
+    // Заблокировать кнопку
+    // Рассчитать ширину прогресс бара
+    fieldsDone () {
+      return this.info.reduce((sum, current) => {
+        const number = current.valid ? 1 : 0
+        return sum + number
+      }, 0)
+    },
+    progressWidth () {
+      const percent = 100 / this.info.length
+      return 'width: ' + this.fieldsDone * percent + '%'
+    },
+    formIsValid () {
+      return this.fieldsDone === this.info.length
+    }
+  },
+  methods: {
+    onInput (value, i) {
+      const field = this.info[i]
+      field.value = value
+      field.isIconVisible = true
+      field.valid = field.pattern.test(field.value)
+    },
+    onSubmit () {
+      this.isSubmited = true
+    }
+  },
+  created () {
+    this.info.forEach(item => {
+      item.isIconVisible = false
+      item.valid = false
+    })
   }
 }
 </script>
@@ -77,9 +102,21 @@ export default {
   text-align: left;
 }
 .progress {
+  position: relative;
   margin-bottom: 6rem;
+}
+.progress-bar {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 0;
+  height: 100%;
+  background-color: darkgreen;
 }
 button {
   margin-bottom: 5rem;
+}
+.fa {
+  margin-left: .4rem;
 }
 </style>
