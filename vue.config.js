ea1fa781c833eff9ffcd6d37d527726bf1613add
serii <seriiburduja@gmail.com>
2021-04-01 13:55:16 +0300

    vue deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-lavrik-base/" : "",
   chainWebpack: (config) => {
     config.module.rules.delete("eslint")
   }
